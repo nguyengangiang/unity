b712325858323689806d0c8c363facbc923663b9
nhim <gn02.nguyen@gmail.com>
2019-05-12 18:33:37 -0700

    Code Text101 trivial game.

---
+++ Text101/Text101/Assets/Text101script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text101script : MonoBehaviour
+{
+    Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Hi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

396b0c3e37cd491d045fceba51addef9d4b5bad3
nhim <gn02.nguyen@gmail.com>
2019-05-12 19:09:48 -0700

    Rename Text101.

--- Text101/Text101/Assets/AdventureGame.cs
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Text101script : MonoBehaviour
+public class AdventureGame : MonoBehaviour
 {
-    Text textComponent;
+    // New instance variable
+    [SerializeField] Text textComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("Hi");
+        textComponent.text = ("I was added programmatically");
     }
 
     // Update is called once per frame

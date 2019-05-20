7f91c4bdae48de4247ae53821567447acf662cc9
nhim <gn02.nguyen@gmail.com>
2019-05-19 20:06:48 -0700

    Implementing code for the Text 101.

--- Text101/Text101/Assets/AdventureGame.cs
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -7,11 +7,18 @@ public class AdventureGame : MonoBehaviour
 {
     // New instance variable
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    // Current state
+    State state;
+    string[] dayOfWeek = { "mon", "tue", "wed", "thu", "fri", "sat", "sun" };
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("I was added programmatically");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        Debug.Log(dayOfWeek[1]);
     }
 
     // Update is called once per frame
